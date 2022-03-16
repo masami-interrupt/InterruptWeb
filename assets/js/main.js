e7a4daa337b9438f5881bea010746500108e0f00
naoko kubo <naochi.interrupt@gmail.com>
2022-03-16 19:13:10 +0900

    指摘事項反映

--- assets/js/main.js
+++ assets/js/main.js
@@ -348,12 +348,12 @@
 				});
 
 	});
-  $('a[href^="#"]').click(function(){
+  $('#sm_more').click(function(){
     var speed = 500;
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href);
     var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
+    $("html, body").delay(10).animate({scrollTop:position}, speed, "swing");
     return false;
   });
 
